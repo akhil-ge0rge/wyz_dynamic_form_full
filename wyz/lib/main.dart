1727a0cdae89c4243fee5608bb5e5bfd8a57599b
Akhil George <akhil8311@gmail.com>
2025-02-10 15:50:34 +0530

    debug banner

--- wyz/lib/main.dart
+++ wyz/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => di<FormProvider>())
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Dynamic Form',
         theme: ThemeData(
           primarySwatch: Colors.blue,
